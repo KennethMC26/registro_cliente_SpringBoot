008d3f80283a004d4830e994ff47ceff22f01508
KennethMC26 <Kmazariegos-2021255@kinal.edu.gt>
2025-08-25 11:55:11 -0600

    no che

---
+++ src/main/java/org/code_wizards/registro_cliente/RegistroClienteWeb.java
@@ -0,0 +1,12 @@
+package org.code_wizards.registro_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class RegistroClienteWeb {
+    public static void main (String [] args){
+        SpringApplication.run(RegistroClienteWeb.class, args);
+    }
+}
