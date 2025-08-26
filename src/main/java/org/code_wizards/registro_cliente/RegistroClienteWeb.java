e23cd554fe7b48bc2804f7179ef2fe68dd48e912
KennethMC26 <Kmazariegos-2021255@kinal.edu.gt>
2025-08-26 08:14:15 -0600

    funciona xd

--- src/main/java/org/code_wizards/registro_cliente/RegistroClienteWeb.java
+++ src/main/java/org/code_wizards/registro_cliente/RegistroClienteWeb.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class RegistroClienteWeb {
     public static void main (String [] args){
         SpringApplication.run(RegistroClienteWeb.class, args);
+        System.out.println("Holi");
     }
 }
