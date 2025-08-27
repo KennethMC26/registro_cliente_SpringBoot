0a006b2ae925c15bd38e62182ad2f7a812a95d96
SuarezGil <Kmazariegos-2021255@kinal.edu.gt>
2025-08-27 07:57:44 -0600

    funcional

--- src/main/java/org/code_wizards/registro_cliente/controller/IndexController.java
+++ src/main/java/org/code_wizards/registro_cliente/controller/IndexController.java
@@ -2,10 +2,13 @@ package org.code_wizards.registro_cliente.controller;
 
 import jakarta.annotation.PostConstruct;
 //import jakarta.faces.view.ViewScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import lombok.Data;
 import org.apache.logging.log4j.message.Message;
 import org.code_wizards.registro_cliente.entity.Cliente;
 import org.code_wizards.registro_cliente.service.IClienteService;
+import org.primefaces.PrimeFaces;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -57,17 +60,17 @@ public class IndexController {
         if (this.clienteSeleccionado.getCodigoCliente()==null){
             this.clienteService.guardarCliente(this.clienteSeleccionado);
             this.clientes.add(this.clienteSeleccionado);
-            //FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cliente agregado"));
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cliente agregado"));
         }
         //modificar
         else{
             this.clienteService.guardarCliente(this.clienteSeleccionado);
-            //FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cliente actualizado"));
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cliente actualizado"));
         }
         //ocultar la ventada modal
         PrimeFaces.current().executeScript("PF('ventanaModalCliente).hide");
         //Actualizar tabla utilizando tecnologia incorporada - AJAx -
-        PrimeFaces.current().ajax.update("formulario-clientes:mensaje-emergente","formulario-clientes:tabla-clientes");
+        PrimeFaces.current().ajax().update("formulario-clientes:mensaje-emergente","formulario-clientes:tabla-clientes");
         //limpiar el objero cliente seleccionado
         this.clienteSeleccionado = null;
     }
@@ -81,6 +84,6 @@ public class IndexController {
         this.clienteSeleccionado = null;
         //confirmar accion
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cliente eliminado"));
-        PrimeFaces.current().ajax.update("formulario-clientes:mensaje-emergente","formulario-clientes:tabla-clientes");
+        PrimeFaces.current().ajax().update("formulario-clientes:mensaje-emergente","formulario-clientes:tabla-clientes");
     }
 }
